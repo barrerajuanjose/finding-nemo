ebd8eef022df7a617290611068f934003e96dca4
Juan Jose Barrera <barrerajuanjose@gmail.com>
2020-05-24 20:48:58 -0300

    fix params (#1)

--- src/nemo/mod.rs
+++ src/nemo/mod.rs
@@ -4,19 +4,26 @@ pub struct Nemo {
     pub search_url: String,
 }
 
-pub fn find_it(site: Option<&String>, mp: Option<&String>, me: Option<&String>, it: Option<&String>) -> Nemo {
-    let mut search = get_host_by_site(site.map_or(String::from("NONE"), |s| s.to_string()));
-
+pub fn find_it(site_param: Option<&String>, mp: Option<&String>, me: Option<&String>, it: Option<&String>) -> Nemo {
+    let site = site_param.map_or(String::from("NONE"), |s| s.to_string());
     let item_type = it.map_or(String::from("NONE"), |s| s.to_string());
     let mercado_pago = mp.map_or(String::from("NONE"), |s| s.to_string());
     let mercado_envios = me.map_or(String::from("NONE"), |s| s.to_string());
 
+    let (q_highest_price, q_lowest_price) = if site.as_str() == "MLB" {
+        ("ferramientas", "livros")
+    } else {
+        ("herramientas", "libros")
+    };
+
+    let mut search = get_host_by_site(site);
+
     match mercado_pago.as_str() {
         "psj" => search += "?q=celulares&installments=no_interest",
-        "nopsj" => search += "?q=celulares&installments=yes",
-        "higestprice" => search += "?q=herramientas&sort=price_desc",
-        "lowerprice" => search += "?q=libros&sort=price_asc",
-        _ => search += "?q?celulares",
+        "installments" => search += "?q=celulares&installments=yes",
+        "highestprice" => search += format!("?q={}&sort=price_desc", q_highest_price).as_str(),
+        "lowestprice" => search += format!("?q={}&sort=price_asc", q_lowest_price).as_str(),
+        _ => search += "?q=celulares",
     }
 
     match item_type.as_str() {
